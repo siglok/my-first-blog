59a7f29c154e694af64f92bf58cf7747bbb0a6c2
Siglok <admin@admin.com>
2018-04-18 18:32:55 -0500

    agregue un cambio pequeño

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url
 from . import views #todas nuestras views del blog
+from django.contrib import admin
 
 urlpatterns=[
-	url(r'^$',views.post_list), 
+	url(r'^$',views.post_list),
+	url(r'^admin/', admin.site.urls), 
 	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail,  name='post_detail'),
 	url(r'^post/new/$', views.post_new, name='post_new'),
 	url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
